c5ae16d7c185639ea7dca1a880d7e7e688243290
王宝明 <wangbaoming@log56.com>
2023-08-24 14:53:41 +0800

    1

---
+++ Example/Pods/Target Support Files/BAlertKit/BAlertKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BAlertKit : NSObject
+@end
+@implementation PodsDummy_BAlertKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-BAlertKit_Tests/Pods-BAlertKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BAlertKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BAlertKit_Tests
+@end
